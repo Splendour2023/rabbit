90373019aa3b51b24ec381f0ae1a8619945a1f8d
Splendour2023 <123588627+Splendour2023@users.noreply.github.com>
2024-12-18 14:18:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Smooth scroll for anchors
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
